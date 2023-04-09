f5ed6b209a1de2a9c69dcccc5c2484fba946283b
Petasco <peterdiyouh@gmail.com>
2023-04-09 00:19:24 +0000

    Initial commit

---
+++ sendorder.php
@@ -0,0 +1,28 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$number = $_POST['number'];
+$foodname = $_POST['foodname'];
+$quantity = $_POST['quantity'];
+$address = $_POST['address'];
+
+// Sanitize the form data to prevent malicious attacks
+$name = htmlspecialchars($name);
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+$number = htmlspecialchars($number);
+$foodname = htmlspecialchars($foodname);
+$quantity = htmlspecialchars($quantity);
+$address = htmlspecialchars($address);
+
+// Send the email
+$to = "peterdiyouh@gmail.com";
+$subject = "New Form Submission";
+$body = "Name: $name\nEmail: $email\nPhone: $number\nFood: $foodname\nQuantity: $quantity\nAddress:\n$address";
+$headers = "From: $email";
+
+if (mail($to, $subject, $body, $headers)) {
+  echo "Thank you for your message!";
+} else {
+  echo "Sorry, there was a problem sending your address.";
+}
+?>

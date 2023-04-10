bfa9436419d66fa6346209b38fbbbf3790ff4478
Petasco <peterdiyouh@gmail.com>
2023-04-10 16:42:41 +0000

    Update sendorder.php

--- sendorder.php
+++ sendorder.php
@@ -1,10 +1,11 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$number = $_POST['number'];
-$foodname = $_POST['foodname'];
-$quantity = $_POST['quantity'];
-$address = $_POST['address'];
+// get the data from the form
+$name = $_POST['Name'];
+$email = $_POST['Email'];
+$number = $_POST['Number'];
+$foodname = $_POST['Foodname'];
+$quantity = $_POST['Quantity'];
+$address = $_POST['Address'];
 
 // Sanitize the form data to prevent malicious attacks
 $name = htmlspecialchars($name);
@@ -18,12 +19,20 @@ $address = htmlspecialchars($address);
 $to = "peterdiyouh@gmail.com";
 $subject = "New Order Submission";
 $body = "Name: $name\nEmail: $email\nPhone: $number\nFood: $foodname\nQuantity: $quantity\nAddress:\n$address";
+// $message = "Name = ". $name. "\r\nEmail = ". $email . "\r\nPhone = ". $number . "\r\nFood = ". $foodname . "\r\nQuuantity = ". $quantity . "\r\nAddress = ". $address;
 $headers = "From: $email";
 
+if ($email != NULL){
+  mail($to, $subject, $body, $headers);
+}
+// redirect after submission
+header("./thanks.html")
+/*
 if (mail($to, $subject, $body, $headers)) {
   // note: you need to have SMTP setup before
   echo "Order Submitted Successfully!";
 } else {
   echo "Sorry, there was a problem sending your order.";
 }
+*/
 ?>

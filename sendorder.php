626558779f691be42350f1067257937407f76add
Petasco <peterdiyouh@gmail.com>
2023-04-20 14:36:31 +0000

    Update sendorder.php

--- sendorder.php
+++ sendorder.php
@@ -1,15 +1,17 @@
 <?php
 $dbhost = 'localhost';
 $dbuser ='root';
-$dbpassword = 'RockMade@1844';
+$dbpassword = '';
 $dbname = 'dborder';
+
+
 // connect database
 $dbConn = new mysqli($dbhost, $dbuser, $dbpassword, $dbname);
-if ($dbConn -> connect_error){
-  die("Sorry... No Connection");
-} else{
-  echo "Success";
-};
+if (!$dbConn){
+  die ("connection failed: ". mysqli_connect_error());
+}
+echo "Database Connected Successfully \n";
+
 // get the data from the form
 $name = $_POST['Name'];
 $email = $_POST['Email'];
@@ -19,20 +21,13 @@ $quantity = $_POST['Quantity'];
 $address = $_POST['Address'];
 
 // insert form data into the database
-$sql = "INSERT INTO orders('Id','Name','Email','Number','Food','Quantity','Address') VALUES ('0', 'name', 'email', 'number', 'foodname', 'quantity', 'address')";
+$sql = "INSERT INTO orders (Name,Email,Number,Food,Quantity,Address) 
+VALUES ('name', 'email', 'number', 'foodname', 'quantity', 'address')";
 
-$rs = mysqli_query($con, $sql);
+$rs = mysqli_query($dbConn, $sql);
 if ($rs){
   echo "Data recorded";
 }
-
-// get the data from the form
-$name = $_POST['Name'];
-$email = $_POST['Email'];
-$number = $_POST['Number'];
-$foodname = $_POST['Foodname'];
-$quantity = $_POST['Quantity'];
-$address = $_POST['Address'];
 
 // Sanitize the form data to prevent malicious attacks
 $name = htmlspecialchars($name);
@@ -46,7 +41,6 @@ $address = htmlspecialchars($address);
 $to = "peterdiyouh@gmail.com";
 $subject = "New Order Submission";
 $body = "Name: $name\nEmail: $email\nPhone: $number\nFood: $foodname\nQuantity: $quantity\nAddress:\n$address";
-// $message = "Name = ". $name. "\r\nEmail = ". $email . "\r\nPhone = ". $number . "\r\nFood = ". $foodname . "\r\nQuuantity = ". $quantity . "\r\nAddress = ". $address;
 $headers = "From: $email";
 
 if ($email != NULL){
@@ -54,6 +48,7 @@ if ($email != NULL){
 }
 // redirect after submission
 header("./thanks.html")
+
 /*
 if (mail($to, $subject, $body, $headers)) {
   // note: you need to have SMTP setup before
